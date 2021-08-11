407a21b192e5ba4c73c8b54a379ffc21796f9292
johnnybueno <johnny.a.bueno1991@gmail.com>
2021-08-11 02:45:13 -0300

    estrutura

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '59a1846c795531a924a3db259233aaed20981907',
+    'name' => 'agenda/agenda',
+  ),
+  'versions' => 
+  array (
+    'agenda/agenda' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '59a1846c795531a924a3db259233aaed20981907',
+    ),
+  ),
+);
